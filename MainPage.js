5968bef5575180ebc2effbe06383ddd2563a1dfc
Bogdan Besedin <bogdan199614@ukr.net>
2016-02-03 20:03:31 +0200

    first commit

---
+++ MainPage.js
@@ -0,0 +1,5 @@
+﻿var PageError = {};
+PageError.PageNotFound = function() {
+    document.write('Данной страницы пока что не существует!<br>');
+    document.write("<a href=\"MainPage.html\">Вернуться на главную страницу</a>");
+}
